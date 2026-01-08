c9e762ac5757c067ce7424e251f1aacf080ab0b9
Yukixx96 <315149625@qq.com>
2026-01-08 15:41:49 +0900

    Create budgets.js

---
+++ budgets.js
@@ -0,0 +1,11 @@
+// 按你最新表：单位“万日元/月”
+export const BUDGETS = [
+  { key:"NASDAQ",   name:"111",     monthlyMan: 5 },
+  { key:"US_TECH",  name:"222", monthlyMan: 4 },
+  { key:"JP_HEAVY", name:"333",   monthlyMan: 3 },
+  { key:"JP_BANK",  name:"444",     monthlyMan: 3 },
+  { key:"JP_SEMI",  name:"555",   monthlyMan: 2 },
+  { key:"JP_GAME",  name:"666", monthlyMan: 1 },
+  { key:"JP_RE",    name:"777",   monthlyMan: 1 },
+  { key:"OTHER",    name:"其他",         monthlyMan: 1 },
+];
